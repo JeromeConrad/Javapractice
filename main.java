a5be9fea098878e4b93c701e5d4e6ed46a5e96db
JeromeConrad <odhiambojeromeconrad@gmail.com>
2026-07-26 02:46:50 +0300

    message

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        
+    }
+}
